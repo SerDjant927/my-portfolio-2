88a257ea64d17fe56458ec81b6b2c92c85eca920
SerDjant927 <sergey.poroshin927@gmail.com>
2019-03-05 11:03:48 +0700

    add map, other important changes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
+//анимация c хедера к элементам//
 $(document).ready(function(){
   $('.menu_link').on('click', function(){
 	var id = $(this).data('link');
 	var block = $('body').find('[data-block="'+id+'"]');
 	var top = block.offset().top;
-    $('html, body').animate({scrollTop: top}, 1500);
+    $('html, body').animate({scrollTop: top}, 1200);
+	return false;
+})
+});
+
+
+//анимация ссылки из текста на портфолио//
+$(document).ready(function(){
+  $('.about-link').on('click', function(){
+	var id = $(this).data('link');
+	var block = $('body').find('[data-block="'+id+'"]');
+	var top = block.offset().top;
+    $('html, body').animate({scrollTop: top}, 1200);
+	return false;
+})
+});
+
+//анимация кнопок наверх//
+$(document).ready(function(){
+  $('.topbtn').on('click', function(){
+	var id = $(this).data('link');
+	var block = $('body').find('[data-block="'+id+'"]');
+	var top = block.offset().top;
+    $('html, body').animate({scrollTop: top}, 1200);
 	return false;
 })
 });
@@ -14,4 +38,6 @@ $(document).ready(function(){
 $('.card').on('click', function() {
     $('.container-portfolio').toggleClass('container-origin');
 });
+
+
 
